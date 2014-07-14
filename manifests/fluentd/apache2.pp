28801131159c6d945c90b025c50280bf57274c2d
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-07-14 08:20:28 +0000

    fixata regexp per matchare più ip

--- manifests/fluentd/apache2.pp
+++ manifests/fluentd/apache2.pp
@@ -6,7 +6,7 @@ class ispconfig_logarchive::fluentd::apache2 {
   fluentd::source { 'apache_main':
     configfile  => 'apache2',
     type        => 'tail',
-    format      => '/^(?<vhost>[^|]*)\|\|\|\|(?<size>[0-9\-]*)\|\|\|\|(?<client>[^ ]*) (?<host>[^ ]*) (?<vhost>[^ ]*) (?<user>[^ ]*) \[(?<time>[^\]]*)\] "(?<method>\S+)(?: +(?<path>[^ ]*) +\S*)?" (?<code>[^ ]*) (?<size>[0-9\-]*)(?: "(?<referer>[^\"]*)" "(?<agent>[^\"]*)")? "(?<cookie>[^\"]*)" (?<serv_time_second>[0-9]*)/(?<serv_time_microsecond>[0-9]*) (?<conn_status>[X|\+|\-]?) (?<process_id>[0-9]*)$/',
+    format      => '/^(?<vhost>[^|]*)\|\|\|\|(?<size>[0-9\-]*)\|\|\|\|(?<client>((.*, )*[^ ]*)) (?<host>[^ ]*) (?<vhost>[^ ]*) (?<user>[^ ]*) \[(?<time>[^\]]*)\] "(?<method>\S+)(?: +(?<path>[^ ]*) +\S*)?" (?<code>[^ ]*) (?<size>[0-9\-]*)(?: "(?<referer>[^\"]*)" "(?<agent>[^\"]*)")? "(?<cookie>[^\"]*)" (?<serv_time_second>[0-9]*)/(?<serv_time_microsecond>[0-9]*) (?<conn_status>[X|\+|\-]?) (?<process_id>[0-9]*)$/',
     time_format => '%d/%b/%Y:%H:%M:%S %z',
     tag         => "${ispconfig_logarchive::destination}.apache.access_log",
     config      => $input_opts,
